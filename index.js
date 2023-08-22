4828552b1b08652e76ac7b4a9e9d19a4510fb490
lorenzo <lorenzomojica561@gmail.com>
2023-08-22 17:41:50 -0400

    adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob" 
+
+function Name () {
+    return customerName
+}
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer () {
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer () {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "enzo"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob"
+}
